c005185a47d96390dc5113fc4a1a6bcaa9b44de5
Iwanow Arsenij <35989@uth.lokalne>
2024-11-13 19:35:25 +0100

    final

---
+++ src/main/java/pl/example/vehicles/Vehicle.java
@@ -0,0 +1,7 @@
+package pl.examples.vehicles;
+public abstract class Vehicle{
+    private String name;
+    public Vehicle(String name) {  this.name = name; }
+    public String getName() { return name; }
+    public abstract int getMaxSpeed();
+}
